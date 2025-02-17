45017217d0be4eb2743b74c773170e455bdea8ab
tamro killas <t.kilasonia2006@gmail.com>
2025-02-17 23:39:35 +0400

    Added index.js as the entry point for the React app

---
+++ index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'; // Optional, if you want to apply global styles
+import App from './App'; // Import the App component
+import reportWebVitals from './reportWebVitals'; // Optional, for performance monitoring
+
+// The root element in the HTML file where React will be rendered
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component into the DOM
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// Optional: Measure performance (you can remove if not needed)
+reportWebVitals();
