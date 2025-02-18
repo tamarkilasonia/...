0ec395d2e707f350d3340095e6cfe5bc8afd4cc3
Killas <t.kilasonia2006@gmail.com>
2025-02-18 20:50:58 +0400

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-// Render the App component into the DOM
+
 root.render(
   <React.StrictMode>
     <App />
